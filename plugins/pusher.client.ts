f83e8a82ea9e313e2b62128f000afe0ccf2eabe2
sial <rimsila.itc@gmail.com>
2022-09-13 18:12:27 +0700

    feat: pusher

--- plugins/pusher.client.ts
+++ plugins/pusher.client.ts
@@ -1,7 +1,7 @@
 import Pusher from 'pusher-js';
 
 export default defineNuxtPlugin(() => {
-  const pusher = new Pusher('2ed3f83182b8766af52b', {
+  const pusher = new Pusher('key', {
     cluster: 'ap1',
     authorizer: ({ name }) => ({
       authorize: async (socketId, callback) => {
@@ -12,7 +12,7 @@ export default defineNuxtPlugin(() => {
         const authRes = await _fetchUtil('', {
           method: 'POST',
           body: formData,
-          baseURL: 'https://api-dev.umpay.io/api/v1/gateway/auth/pusher',
+          baseURL: 'api/gateway/auth/pusher',
         });
 
         callback(
